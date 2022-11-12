5c90730e7595d85691cac5018262c57acc23d728
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-11-11 18:47:44 -0600

    working on vertexes

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 import * as THREE from 'three'
 import styles from './style.css'
+import vertexShader from './shaders/vertex.glsl'
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 )
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true})
 
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 //Beginning of sphere
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({color: 0xFF0000}))
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), 
+new THREE.ShaderMaterial({ vertexShader: , fragmentShader: })
+)
 
 console.log(sphere)
 
 scene.add(sphere)
 
-camera.position.z = 10
+camera.position.z = 15
 
 
 function animate() {
